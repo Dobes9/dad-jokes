dd829ba449a6fb84e586edb1ba511c38fc1b6acc
Richard H Dobeck <rdobeck@yahoo.com>
2022-01-22 13:21:10 -0800

    added upvote and downvote handlers, bound to component, onClick event listeners for arrow icons call appropriate handlers

--- src/jokes/Joke.js
+++ src/jokes/Joke.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
 
 class Joke extends Component {
+  constructor(props) {
+    super(props);
+    this.handleUpVote = this.handleUpVote.bind(this);
+    this.handleDownVote = this.handleDownVote.bind(this);
+  }
+  handleUpVote() {
+    const { handleVote, id } = this.props;
+    handleVote(id, 1);
+  }
+  handleDownVote() {
+    const { handleVote, id } = this.props;
+    handleVote(id, -1);
+  }
   render() {
+    const { votes, joke } = this.props;
     return (
       <div className="Joke">
         <div className="Joke-buttons">
-          <i className="fas fa-arrow-up"></i>
-          <span>{this.props.votes}</span>
-          <i className="fas fa-arrow-down"></i>
+          <i className="fas fa-arrow-up" onClick={this.handleUpVote}></i>
+          <span>{votes}</span>
+          <i className="fas fa-arrow-down" onClick={this.handleDownVote}></i>
         </div>
+        <div className="Joke-text">{joke}</div>
       </div>
     );
   }
