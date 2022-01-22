c6ff23a31f104837eb07fc5f5b86c28f41c904b5
Richard H Dobeck <rdobeck@yahoo.com>
2022-01-22 14:07:08 -0800

    changed import from helpers to getColorAndEmoji, style in span and class for emoji icon call this function as necessary

--- src/jokes/Joke.js
+++ src/jokes/Joke.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { getColor } from "../utils/helpers";
+import { getColorAndEmoji } from "../utils/helpers";
 import "./Joke.css";
 
 class Joke extends Component {
@@ -22,18 +22,17 @@ class Joke extends Component {
       <div className="Joke">
         <div className="Joke-buttons">
           <i className="fas fa-arrow-up" onClick={this.handleUpVote}></i>
-          <span style={{ borderColor: getColor(votes) }} className="Joke-votes">
+          <span
+            style={{ borderColor: getColorAndEmoji(votes).color }}
+            className="Joke-votes"
+          >
             {votes}
           </span>
           <i className="fas fa-arrow-down" onClick={this.handleDownVote}></i>
         </div>
         <div className="Joke-text">{joke}</div>
         <div className="Joke-emoji">
-          <i
-            className="em em-rolling_on_the_floor_laughing"
-            aria-role="presentation"
-            aria-label="ROLLING ON THE FLOOR LAUGHING"
-          ></i>
+          <i className={getColorAndEmoji(votes).emoji}></i>
         </div>
       </div>
     );
