8a4c76aef89e67b48e1d84499cbe3a5df05d5bbe
Richard H Dobeck <rdobeck@yahoo.com>
2022-01-22 12:42:26 -0800

    added sidebar, image in sidebar, and button that will generate new jokes

--- src/jokes/JokeList.js
+++ src/jokes/JokeList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Joke from "./Joke";
 import axios from "axios";
+import "./JokeList.css";
 import { loadJokes } from "../utils/apiCalls";
 
 class JokeList extends Component {
@@ -20,7 +21,14 @@ class JokeList extends Component {
   render() {
     return (
       <div className="JokeList">
-        <h1>Dad Jokes</h1>
+        <div className="JokeList-sidebar">
+          <h1 className="JokeList-title">
+            <span>Dad</span> Jokes
+          </h1>
+          <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"></img>
+          <button className="JokeList-getmore">New Jokes</button>
+        </div>
+
         <div className="JokeList-jokes">
           <ul>
             {this.state.jokes.map((joke) => {
