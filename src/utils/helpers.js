e0f36d4c63e4156479a0040b84b4204bb95d339a
Richard H Dobeck <rdobeck@yahoo.com>
2022-01-22 13:57:17 -0800

    added and exporting getEmoji function, which returns a string determining which emoji to use based on num of votes

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -15,3 +15,21 @@ export function getColor(votes) {
     return "#f44336";
   }
 }
+
+export function getEmoji(votes) {
+  if (votes >= 15) {
+    return "em em-rolling_on_the_floor_laughing";
+  } else if (votes >= 12) {
+    return "em em-laughing";
+  } else if (votes >= 9) {
+    return "em em-smiley";
+  } else if (votes >= 6) {
+    return "em em-slightly_smiling_face";
+  } else if (votes >= 3) {
+    return "em em-neutra_face";
+  } else if (votes >= 0) {
+    return "em em-confused";
+  } else {
+    return "em em-angry";
+  }
+}
