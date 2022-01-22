0f6fd7272a0c03e533cdf2c58a38101c29c3ff87
Richard H Dobeck <rdobeck@yahoo.com>
2022-01-22 15:57:26 -0800

    loadJokes now accepts a set as a param, only adds joke to array if set does not contain that joke

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -3,13 +3,19 @@ import { v4 as uuidv4 } from "uuid";
 
 const BASE_API_URL = "https://icanhazdadjoke.com/";
 
-export async function loadJokes(numJokes) {
+export async function loadJokes(numJokes, seenJokes) {
   let jokes = [];
   while (jokes.length < numJokes) {
     const response = await axios.get(BASE_API_URL, {
       headers: { accept: "application/json" },
     });
-    jokes.push({ joke: response.data.joke, id: uuidv4(), votes: 0 });
+    const { joke } = response.data;
+    if (!seenJokes.has(joke)) {
+      jokes.push({ joke, id: uuidv4(), votes: 0 });
+    } else {
+      console.log("FOUND A DUPLICATE!");
+      console.log(joke);
+    }
   }
   return jokes;
 }
